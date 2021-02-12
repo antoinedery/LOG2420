60cf667804737da1612eb1754871e1448af842bb
IamStephy <65076512+IamStephy@users.noreply.github.com>
2021-02-12 02:53:53 -0500

    Modal for nouvelle1 is in progress

---
+++ TP1/NouvelleVersion/js/script.js
@@ -0,0 +1,25 @@
+
+
+/*
+const openModal = document.getElementById("[data-model-target]");
+const closeModalButton = document.getElementById("[data-close-button]");
+
+openModal.foreach(buttons => {
+    buttons.addEventListerner('click', () => { 
+        const modal = document.querySelectorAll(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButton.foreach(buttons => {
+    buttons.addEventListerner('click', () => { 
+        const modal = button.closet('.modal')
+        closeModalButton(modal)
+    })
+})
+
+function openModal(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+}
+*/
