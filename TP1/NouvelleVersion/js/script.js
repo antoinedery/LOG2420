5ac6caf4437bdaaea0a21566e430e64bf6c81d69
Antoine Déry <67334691+antoinedery12@users.noreply.github.com>
2021-02-16 22:56:00 -0500

    Ajout des inputs box + fleches dans la section commande du materiel

--- TP1/NouvelleVersion/js/script.js
+++ TP1/NouvelleVersion/js/script.js
@@ -12,3 +12,24 @@ function closeModal(modal){
     if(modal == nul) return 
     modal.remove('active')
 }
+
+/*Pour incrémenter le compteur dans la section Commande du matériel*/
+function incrementCounter(id)
+{
+    var value = parseInt(document.getElementById(id).value);
+    value = isNaN(value) ? 0 : value;
+  	value++;
+  	document.getElementById(id).value = value;
+}
+
+/*Pour décrémenter le compteur dans la section Commande du matériel*/
+function decrementCounter(id)
+{
+  var value = parseInt(document.getElementById(id).value);
+  value = isNaN(value) ? 0 : value;
+  if (value != 0) //Evite d'afficher des valeurs négatives
+  {
+  	value--;
+  }
+  document.getElementById(id).value = value;
+}
